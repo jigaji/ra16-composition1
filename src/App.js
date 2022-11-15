906980bc0c48279f0f208b3787dccbfd9e26e7d7
jigaji <ts.saglara@gmail.com>
2022-11-15 18:14:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Cards from './components/Cards';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Cards image="https://i.pinimg.com/736x/fd/94/9e/fd949e0306d13356343a8715cf26ab68.jpg">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#1" className="btn btn-primary">Go somewhere</a>
+      </Cards>
+      <Cards>
+        <h5 className="card-title">Special title title treatment</h5>
+        <p className="card-text">With supportintg text bellow as a natural lead-in to additional content.</p>
+        <a href="#1" className="btn btn-primary">Go somewhere</a>
+      </Cards>
     </div>
   );
 }
